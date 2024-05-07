3affe8b54d770c86d94a9ff050301d246eb7138c
Sourabh <sourabh4pvt@gmail.com>
2024-05-08 00:01:44 +0530

    removed unnecessary index.js export files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { TodoProvider } from "./context";
-import { TodoForm, TodoItem } from './components/index'
+import { TodoProvider } from "./context/TodoContext";
+// import { TodoForm, TodoItem } from './components/index'
+import TodoItem from "./components/TodoItem";
+import TodoForm from "./components/TodoForm";
 import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
@@ -52,7 +54,7 @@ function App() {
     >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-lg rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+          <h1 className="text-3xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
           <div className="mb-4">
